405e05cd1f373299dfccecd81f504bf30bb8cdeb
Eduardo Gutierrez <eduardo@reversethreaded.com>
2020-05-22 12:54:54 -0400

    Test accessing variable sets with `#[]`

--- test/warg/config_test.rb
+++ test/warg/config_test.rb
@@ -20,4 +20,16 @@ class WargConfigTest < Minitest::Test
     assert_equal "/srv/yummy-only", config.deploy.path
     assert_equal Time.new(2020, 4, 20, 4, 20), config.deploy.timestamp
   end
+
+  def test_hash_like_access_for_variable_sets
+    config = Warg::Config.new
+
+    config.variables(:demo) do |demo|
+      demo.probability = 0.3
+    end
+
+    assert_equal 0.3, config[:demo].probability
+    assert_equal 0.3, config["demo"].probability
+    assert_nil config["nada"]
+  end
 end
