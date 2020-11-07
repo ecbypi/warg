c9e9a0a69c368726fc2b6937e1a63e96b6eb6ede
Eduardo Gutierrez <eduardo@velvetsocks.com>
2020-11-06 23:54:21 -0500

    Test template lookup without a matching extension

--- test/warg/template_test.rb
+++ test/warg/template_test.rb
@@ -1,6 +1,19 @@
 require "test_helper"
 
 class WargScriptTemplateTest < Minitest::Test
+  def setup
+    Warg.search_paths.unshift Warg::Testing.dummy_directory
+  end
+
+  def teardown
+    Warg.search_paths.clear
+  end
+
+  def test_finds_with_or_without_extension
+    assert Warg::Script::Template.find("process-snapshot.sh")
+    assert Warg::Script::Template.find("process-snapshot")
+  end
+
   def test_fails_by_default_on_lookup
     assert_raises(RuntimeError) { Warg::Script::Template.find("no-se-encuentra") }
   end
